d3826f05e95e610ae59364bfc78d080e13d27b33
rafasponte <129603992+rafasponte@users.noreply.github.com>
2024-01-26 20:14:25 +0000

    Need to Fix card drop area

--- Scenes/card_ui/card_ui.gd
+++ Scenes/card_ui/card_ui.gd
@@ -5,12 +5,36 @@ signal reparent_requested(which_card_ui: CardUI)
 
 @onready var color: ColorRect = $Color
 @onready var state: Label = $State
+@onready var drop_point_detector: Area2D = $DropPointDetector
+@onready var card_state_machine: CardStateMachine = $CardStateMachine as CardStateMachine
+@onready var targets: Array[Node] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	card_state_machine.init(self)
+
+func _input(event: InputEvent) -> void:
+	card_state_machine.on_input(event)
+
+func _on_gui_input(event: InputEvent) -> void:
+	card_state_machine.on_gui_input(event)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_mouse_entered() -> void:
+	card_state_machine.on_mouse_entered()
+
+
+func _on_mouse_exited() -> void:
+	card_state_machine.on_mouse_exited()
+
+
+
+
+func _on_drop_point_detector_area_entered(area: Area2D) -> void:
+	print("Here")
+	if not targets.has(area):
+		targets.append(area)
+
+
+func _on_drop_point_detector_area_exited(area: Area2D) -> void:
+	targets.erase(area)
